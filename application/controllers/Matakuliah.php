375dbb8a667198d0dfbb65967edabfb26ec1c21a
NurzatiSudrajat <15220187@bsi.ac.id>
2024-04-18 20:46:24 +0700

    Menambahkan Tugas Pertemuan 5

--- application/controllers/Matakuliah.php
+++ application/controllers/Matakuliah.php
@@ -24,7 +24,7 @@ class Matakuliah extends CI_Controller
             'nama' => $this->input->post('nama'),
             'sks'  => $this->input->post('sks')
         ];
-        $this->load->library('form_validation');
+        
         $this->load->view('view-data-matakuliah', $data);
         }
     }
